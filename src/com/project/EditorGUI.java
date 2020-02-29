0fded75c5553aab970249c5ce6b147fe01484341
kandel_k <kirill.kandel@gmail.com>
2020-02-29 13:52:30 +0200

    Added buttons for search, made small fixes with saving

--- src/com/project/EditorGUI.java
+++ src/com/project/EditorGUI.java
@@ -12,13 +12,17 @@ class EditorGUI extends JFrame{
     private JTextField fileName = new JTextField();
     private JButton saveButton = new JButton(new ImageIcon("resources/save.png"));
     private JButton loadButton = new JButton(new ImageIcon("resources/load.png"));
+    private JButton searchButton = new JButton(new ImageIcon("resources/search.jpg"));
+    private JButton nextButton = new JButton(new ImageIcon("resources/next.jpg"));
+    private JButton prevButton = new JButton(new ImageIcon("resources/prev.jpg"));
     private JMenu fileMenu = new JMenu("File");
     private JMenuItem newMenuItem = new JMenuItem("New");
     private JMenuItem loadMenuItem = new JMenuItem("Open");
     private JMenuItem saveMenuItem = new JMenuItem("Save");
     private JMenuItem exitMenuItem = new JMenuItem("Exit");
-    private JMenuBar jMenuBar = new JMenuBar();
     private JMenuItem saveAsMenuItem = new JMenuItem("Save as");
+    private JMenuBar jMenuBar = new JMenuBar();
+    private JCheckBox checkBox = new JCheckBox("Use regexp");
 
     private File currDir = new File(System.getProperty("user.dir"));
     private JFileChooser fileChooser = new JFileChooser(currDir);
@@ -32,6 +36,8 @@ class EditorGUI extends JFrame{
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 
         jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
@@ -49,6 +55,7 @@ class EditorGUI extends JFrame{
         saveMenuItem.setName("MenuSave");
         exitMenuItem.setName("MenuExit");
         newMenuItem.setName("MenuNew");
+        saveAsMenuItem.setName("MenuSaveAs");
 
         createLayout();
         createMenu();
@@ -66,7 +73,11 @@ class EditorGUI extends JFrame{
                 .addGroup(groupLayout.createParallelGroup()
                     .addComponent(saveButton,24,24,24)
                     .addComponent(loadButton,24,24,24)
-                    .addComponent(fileName, 24, 24, 24))
+                    .addComponent(fileName, 24, 24, 24)
+                    .addComponent(searchButton, 24, 24, 24)
+                    .addComponent(prevButton, 24, 24, 24)
+                    .addComponent(nextButton, 24, 24, 24)
+                    .addComponent(checkBox))
                 .addComponent(jScrollPane)
         );
 
@@ -74,7 +85,11 @@ class EditorGUI extends JFrame{
                 .addGroup(groupLayout.createSequentialGroup()
                     .addComponent(saveButton)
                     .addComponent(loadButton)
-                    .addComponent(fileName))
+                    .addComponent(fileName)
+                    .addComponent(searchButton)
+                    .addComponent(prevButton)
+                    .addComponent(nextButton)
+                    .addComponent(checkBox))
                 .addComponent(jScrollPane)
         );
     }
@@ -88,16 +103,18 @@ class EditorGUI extends JFrame{
         fileMenu.add(newMenuItem);
         fileMenu.add(saveMenuItem);
         fileMenu.add(loadMenuItem);
-        fileName.add(saveAsMenuItem);
+        fileMenu.add(saveAsMenuItem);
         fileMenu.addSeparator();
         fileMenu.add(exitMenuItem);
     }
 
     private void createActionListeners() {
         ActionListener saveListener = e -> {
-            if (!new FileEditor().saveFile(fileChooser.getSelectedFile().getAbsolutePath(), jTextArea.getText())) {
-                JOptionPane.showMessageDialog(null, "Error in saving file", "Error", JOptionPane.ERROR_MESSAGE);
+            if (fileChooser.getSelectedFile() == null && !chooseSaveDir()) {
+                return;
             }
+
+            saveFile();
         };
 
         ActionListener loadListener = e -> {
@@ -105,13 +122,13 @@ class EditorGUI extends JFrame{
                 fileChooser.setCurrentDirectory(currDir);
                 return;
             }
-
             currDir = fileChooser.getCurrentDirectory();
             fileChooser.setCurrentDirectory(currDir);
 
             String loadedFile = new FileEditor().loadFile(fileChooser.getSelectedFile().getAbsolutePath());
             if (loadedFile == null) {
                 JOptionPane.showMessageDialog(null, "File not found", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
             }
 
             jTextArea.setText(loadedFile);
@@ -127,14 +144,32 @@ class EditorGUI extends JFrame{
 
         saveMenuItem.addActionListener(saveListener);
 
-        //TODO add action listener for save as
         saveAsMenuItem.addActionListener(e -> {
-
+            if (chooseSaveDir()) {
+                saveFile();
+            }
         });
 
         newMenuItem.addActionListener(e -> {
             jTextArea.setText("");
-            fileName.setText("newFile.txt");
+            fileChooser.setSelectedFile(null);
         });
+    }
+
+    private boolean chooseSaveDir() {
+        if (fileChooser.showSaveDialog(null) == JFileChooser.CANCEL_OPTION) {
+            fileChooser.setCurrentDirectory(currDir);
+            return false;
+        }
+        currDir = fileChooser.getCurrentDirectory();
+        fileChooser.setCurrentDirectory(currDir);
+
+        return  true;
+    }
+
+    private void saveFile() {
+        if (!new FileEditor().saveFile(fileChooser.getSelectedFile().getAbsolutePath(), jTextArea.getText())) {
+            JOptionPane.showMessageDialog(null, "Error in saving file", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
